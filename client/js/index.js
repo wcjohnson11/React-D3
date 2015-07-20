f3736c0c5b91c350005e31457c8cc8809f4d4b56
wcjohnson11 <williamcouperjohnson@gmail.com>
2015-07-19 19:11:41 -0700

    adding flux

---
+++ client/js/index.js
@@ -0,0 +1,8 @@
+var React = require('react/addons');
+// components
+var SegmentApp = require('./components/SegmentApp.jsx');
+
+React.render(
+  <SegmentApp />,
+  document.getElementById('main')
+);
