a448cca31a335267146a1ebc2c6360313f843386
wcjohnson11 <williamcouperjohnson@gmail.com>
2015-07-22 22:03:19 -0700

    begin segmentApp

---
+++ client/js/components/SegmentApp.jsx
@@ -0,0 +1,19 @@
+var React = require('react/addons');
+var cx = React.addons.classSet;
+var _ = require('lodash');
+var d3 = require('d3/d3');
+
+var SegmentApp = React.createClass({
+  getInitialState() {
+    return {
+    }
+  },
+  componentDidMount() {
+  },
+  componentWillUnMount() {
+  },
+  render() {
+  },
+});
+
+module.exports = SegmentApp;
